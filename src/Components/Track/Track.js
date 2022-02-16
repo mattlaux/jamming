127e4ce9a9fb36200a542361ce213a06f758904d
drattylight <mlaux6@outlook.com>
2022-02-16 10:24:49 -0600

    Create Track component and add CSS

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Track.css';
+
+export class Track extends React.Component {
+    
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <button class="Track-action">-</button>
+        } else {
+            return <button class="Track-action">+</button>
+        }
+    }
+    
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                { this.renderAction() }
+            </div>
+        )
+    };
+};
