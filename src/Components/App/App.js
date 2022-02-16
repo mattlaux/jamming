c977fd12976354cb02da5c39910a93386cbb2778
drattylight <mlaux6@outlook.com>
2022-02-16 13:30:55 -0600

    Add song addition and removal functionality from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
         {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
         {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
         {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
+        {name: 'name4', artist: 'artist4', album: 'album4', id: 4},
       ],
       playlistName: 'Classics',
       playlistTracks: [
@@ -20,6 +21,23 @@ class App extends React.Component {
         {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  };
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  };
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks: tracks});
   };
   
   render() {
@@ -29,8 +47,13 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <SearchResults 
+              searchResults={this.state.searchResults} 
+              onAdd={this.addTrack} />
+            <Playlist 
+              playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
